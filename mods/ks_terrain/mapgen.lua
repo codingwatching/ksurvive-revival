8695161547259fba0e322b0d50c63e4138ed6573
Eric Rustrum <rustrumek@gmail.com>
2021-08-26 10:50:10 -0700

    Added comment, fixed bug.

--- mods/ks_terrain/mapgen.lua
+++ mods/ks_terrain/mapgen.lua
@@ -6,7 +6,9 @@ minetest.register_alias("mapgen_water_source", "ks_terrain:water_source")
 
 -- Now, I am registering the nodes needed for making grassland biomes.
 minetest.register_alias("biomes_topsoil", "ks_terrain:topsoil")
+minetest.register_alias("biomes_subsoil", "ks_terrain:subsoil")
 minetest.register_alias("biomes_topsoil_grass", "ks_terrain:topsoil_with_grass")
+
 
 
 -- Jetzt ich bin... I'm just gonna register biomes now.
@@ -15,7 +17,8 @@ minetest.register_biome({
 	node_top = "biomes_topsoil_grass",
 	depth_top = 1,
 	node_filler = "biomes_topsoil",
-	depth_top = 3,
+	depth_filler = 3,
+	-- try to add subsoil here
 	heat_point = 0,
 	humidity_point = 50,
 })
