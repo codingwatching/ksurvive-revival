2c732785b1b1391a54fbce8ca32cfe34da417456
Eric Rustrum <rustrumek@gmail.com>
2021-08-26 07:57:38 -0700

    Added water node.

--- mods/ks_terrain/nodes.lua
+++ mods/ks_terrain/nodes.lua
@@ -4,3 +4,11 @@ minetest.register_node("ks_terrain:stone", {
 	groups = {rock = 1},
 	is_ground_content = true
 })
+
+
+minetest.register_node("ks_terrain:source_water", {
+	description = "Water Source",
+	tiles = {"terrain.source_water.png"},
+	groups = {water = 1},
+	is_ground_content = true
+})
