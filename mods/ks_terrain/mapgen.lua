59d0c8901648410b61162258a427b4838dcc542f
Eric Rustrum <rustrumek@gmail.com>
2021-08-26 08:49:08 -0700

    Changed mapgen to take dolomite as surface rock.
    I will add a strata of another kind of rock later on.

--- mods/ks_terrain/mapgen.lua
+++ mods/ks_terrain/mapgen.lua
@@ -1,6 +1,6 @@
 
 -- I am registering the two strictly needed mapgen aliases
-minetest.register_alias("mapgen_stone", "ks_terrain:stone")
+minetest.register_alias("mapgen_stone", "ks_terrain:dolomite")
 minetest.register_alias("mapgen_water_source", "ks_terrain:water_source")
 
 
