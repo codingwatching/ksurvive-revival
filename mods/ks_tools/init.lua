e87c49dbef84e1b8c4f79f1494297137496bfd86
Eric Rustrum <rustrumek@gmail.com>
2021-08-27 14:56:14 -0700

    Added stone tool.
    Can dig lots of nodes.

--- mods/ks_tools/init.lua
+++ mods/ks_tools/init.lua
@@ -21,14 +21,17 @@ minetest.register_alias("devtool", "ks_tools:devtool")
 minetest.register_alias("coe", "chest_of_everything:chest")
 
 
-minetest.register_tool("ks_tools:woodtool", {
-	description = "Wooden Tool",
-	inventory_image = "devtool2.png",
+minetest.register_tool("ks_tools:stonetool", {
+	description = "Stone Tool",
+	inventory_image = "tools.stone_pick.png",
 	tool_capabilities = {
 		full_punch_interval = 1.0,
 		max_drop_level=3,
 		groupcaps={
-			chippable={times={[1]=1.00}, maxlevel=1}
+			chippable={times={[1]=tool_damage}, maxlevel=1},
+			diggable={times={[1]=tool_damage}, maxlevel=1},
+			choppable={times={[1]=tool_damage, [2]=tool_damage*2}, maxlevel=2},
+			sliceable={times={[1]=tool_damage}, maxlevel=1},
 		},
 		damage_groups = {fleshy=tool_damage},
 	},
