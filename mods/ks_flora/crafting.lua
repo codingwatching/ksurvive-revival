c2ddb3f108b096120cdc240d75c7648d5e856ee9
Eric Rustrum <rustrumek@gmail.com>
2021-08-28 11:14:07 -0700

    Changed planks crafting recipes.

--- mods/ks_flora/crafting.lua
+++ mods/ks_flora/crafting.lua
@@ -6,10 +6,18 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
+	type = "shapeless",
+	recipe = {
+		{"ks_flora:juniper_stick", "ks_flora:juniper_stick", "ks_flora:juniper_stick", "ks_flora:juniper_stick"}
+	},
+	output = "ks_tools:juniper_planks"
+})
+
+minetest.register_craft({
 	recipe = {
 		{"ks_flora:juniper_log"}
 	},
-	output = "ks_flora:juniper_planks"
+	output = "ks_flora:juniper_planks 2"
 })
 
 
@@ -22,8 +30,16 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
+	type = "shapeless",
+	recipe = {
+		{"ks_flora:maple_stick", "ks_flora:maple_stick", "ks_flora:maple_stick", "ks_flora:maple_stick"}
+	},
+	output = "ks_tools:maple_planks"
+})
+
+minetest.register_craft({
 	recipe = {
 		{"ks_flora:maple_log"}
 	},
-	output = "ks_flora:maple_planks"
+	output = "ks_flora:maple_planks 2"
 })
