2061b20a543c6e2420161ee135f76750cfcd33a7
Eric Rustrum <rustrumek@gmail.com>
2021-08-28 11:15:32 -0700

    Removed boken recipe.

--- mods/ks_flora/crafting.lua
+++ mods/ks_flora/crafting.lua
@@ -6,12 +6,6 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	type = "shapeless",
-	recipe = {"ks_flora:juniper_stick", "ks_flora:juniper_stick", "ks_flora:juniper_stick", "ks_flora:juniper_stick"},
-	output = "ks_tools:juniper_planks"
-})
-
-minetest.register_craft({
 	recipe = {
 		{"ks_flora:juniper_log"}
 	},
@@ -25,12 +19,6 @@ minetest.register_craft({
 		{"ks_flora:maple_planks"}
 	},
 	output = "ks_tools:maple_stick 4"
-})
-
-minetest.register_craft({
-	type = "shapeless",
-	recipe = {"ks_flora:maple_stick", "ks_flora:maple_stick", "ks_flora:maple_stick", "ks_flora:maple_stick"},
-	output = "ks_tools:maple_planks"
 })
 
 minetest.register_craft({
