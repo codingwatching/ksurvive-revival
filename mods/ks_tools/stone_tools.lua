98ecbe2affcb40c65a609f92397c3042654a9366
Eric Rustrum <rustrumek@gmail.com>
2021-08-30 14:33:53 -0700

    Added stone tools with holly sticks

--- mods/ks_tools/stone_tools.lua
+++ mods/ks_tools/stone_tools.lua
@@ -28,6 +28,20 @@ minetest.register_tool("ks_tools:douglasfir_dolomite_pickaxe", {
 	visual_scale = 1.69,
 })
 
+minetest.register_tool("ks_tools:holly_dolomite_pickaxe", {
+	description = "Dolomite Pickaxe",
+	inventory_image = "tools.tool_dolomite_pickaxe_holly.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			chippable={times={[1]=tool_damage*2, [2]=tool_damage*4}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+})
+
 
 
 minetest.register_tool("ks_tools:juniper_dolomite_shovel", {
@@ -47,6 +61,20 @@ minetest.register_tool("ks_tools:juniper_dolomite_shovel", {
 minetest.register_tool("ks_tools:douglasfir_dolomite_shovel", {
 	description = "Dolomite Shovel",
 	inventory_image = "tools.tool_dolomite_shovel_douglasfir.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			diggable={times={[1]=tool_damage, [2]=tool_damage*2}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+})
+
+minetest.register_tool("ks_tools:holly_dolomite_shovel", {
+	description = "Dolomite Shovel",
+	inventory_image = "tools.tool_dolomite_shovel_holly.png",
 	tool_capabilities = {
 		full_punch_interval = 1.0,
 		max_drop_level=1,
@@ -78,6 +106,21 @@ minetest.register_tool("ks_tools:juniper_dolomite_axe", {
 minetest.register_tool("ks_tools:douglasfir_dolomite_axe", {
 	description = "Dolomite Axe",
 	inventory_image = "tools.tool_dolomite_axe_douglasfir.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			choppable={times={[1]=tool_damage, [2]=tool_damage*2}, uses=tool_durability, maxlevel=1},
+			sliceable={times={[1]=tool_damage/4, [2]=tool_damage/2}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+})
+
+minetest.register_tool("ks_tools:holly_dolomite_axe", {
+	description = "Dolomite Axe",
+	inventory_image = "tools.tool_dolomite_axe_holly.png",
 	tool_capabilities = {
 		full_punch_interval = 1.0,
 		max_drop_level=1,
