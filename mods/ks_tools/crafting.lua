9c05dfa2b61c21b4dd4f42adb8461821276e128c
Eric Rustrum <rustrumek@gmail.com>
2021-08-30 14:35:33 -0700

    Added crafting recipe for holly tools.

--- mods/ks_tools/crafting.lua
+++ mods/ks_tools/crafting.lua
@@ -17,6 +17,15 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
+	recipe = {
+		{"ks_tools:holly_stick", "ks_tools:holly_stick"},
+		{"                    ", "ks_tools:holly_stick"},
+		{"                    ", "ks_tools:holly_stick"}
+	},
+	output = "ks_tools:juniper_adze"
+})
+
+minetest.register_craft({
 	type = "shapeless",
 	recipe = {"ks_tools:dolomite_rock", "ks_tools:dolomite_rock"},
 	output = "ks_tools:dolomite_sharpened_rock"
@@ -60,6 +69,14 @@ minetest.register_craft({
 
 minetest.register_craft({
 	type = "shapeless",
+	recipe = {"ks_tools:toolhead_dolomite_pickaxe", "ks_tools:holly_stick"},
+	output = "ks_tools:holly_dolomite_pickaxe"
+})
+
+
+
+minetest.register_craft({
+	type = "shapeless",
 	recipe = {"ks_tools:toolhead_dolomite_shovel", "ks_tools:juniper_stick"},
 	output = "ks_tools:juniper_dolomite_shovel"
 })
@@ -72,6 +89,14 @@ minetest.register_craft({
 
 minetest.register_craft({
 	type = "shapeless",
+	recipe = {"ks_tools:toolhead_dolomite_shovel", "ks_tools:holly_stick"},
+	output = "ks_tools:holly_dolomite_shovel"
+})
+
+
+
+minetest.register_craft({
+	type = "shapeless",
 	recipe = {"ks_tools:toolhead_dolomite_axe", "ks_tools:juniper_stick"},
 	output = "ks_tools:juniper_dolomite_axe"
 })
@@ -80,4 +105,10 @@ minetest.register_craft({
 	type = "shapeless",
 	recipe = {"ks_tools:toolhead_dolomite_axe", "ks_tools:douglasfir_stick"},
 	output = "ks_tools:douglasfir_dolomite_axe"
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	recipe = {"ks_tools:toolhead_dolomite_axe", "ks_tools:holly_stick"},
+	output = "ks_tools:holly_dolomite_axe"
 })
