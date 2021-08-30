a639a9deab56789673f7dc5e05884970e06decb4
Eric Rustrum <rustrumek@gmail.com>
2021-08-30 08:44:23 -0700

    Added recipes for bins.

---
+++ mods/ks_decor/crafting.lua
@@ -0,0 +1,26 @@
+minetest.register_craft({
+	recipe = {
+		{"ks_flora:juniper_planks", "                       ", "ks_flora:juniper_planks"},
+		{"ks_flora:juniper_planks", "                       ", "ks_flora:juniper_planks"},
+		{"ks_flora:juniper_planks", "ks_flora:juniper_planks", "ks_flora:juniper_planks"},
+	},
+	output = "ks_decor:juniper_bin 2"
+})
+
+minetest.register_craft({
+	recipe = {
+		{"ks_flora:holly_planks", "                     ", "ks_flora:holly_planks"},
+		{"ks_flora:holly_planks", "                     ", "ks_flora:holly_planks"},
+		{"ks_flora:holly_planks", "ks_flora:holly_planks", "ks_flora:holly_planks"},
+	},
+	output = "ks_decor:holly_bin 2"
+})
+
+minetest.register_craft({
+	recipe = {
+		{"ks_flora:douglasfir_planks", "                          ", "ks_flora:douglasfir_planks"},
+		{"ks_flora:douglasfir_planks", "                          ", "ks_flora:douglasfir_planks"},
+		{"ks_flora:douglasfir_planks", "ks_flora:douglasfir_planks", "ks_flora:douglasfir_planks"},
+	},
+	output = "ks_decor:douglasfir_bin 2"
+})
