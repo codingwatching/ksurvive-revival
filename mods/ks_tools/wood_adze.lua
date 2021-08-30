e8982c529ca5c2d2c2f2a49a94c2b8fd905f5f0b
Eric Rustrum <rustrumek@gmail.com>
2021-08-30 14:33:51 -0700

    Added Holly Adze.

--- mods/ks_tools/wood_adze.lua
+++ mods/ks_tools/wood_adze.lua
@@ -31,3 +31,19 @@ minetest.register_tool("ks_tools:juniper_adze", {
 	},
 	visual_scale = 1.69,
 })
+
+minetest.register_tool("ks_tools:holly_adze", {
+	description = "Adze",
+	inventory_image = "tools.holly_adze.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			diggable={times={[1]=tool_damage*2, [2]=tool_damage*4}, uses=tool_durability, maxlevel=1},
+			choppable={times={[1]=tool_damage*5, [2]=tool_damage*10}, uses=tool_durability, maxlevel=2},
+			sliceable={times={[1]=tool_damage/2, [2]=tool_damage}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+})
