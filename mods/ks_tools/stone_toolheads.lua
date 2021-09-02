d151e9ac96e859641375b8d7df0b0d20829ba1f5
Eric Rustrum <rustrumek@gmail.com>
2021-09-02 14:45:05 -0700

    Added aragonite tool heads.
    Also added groups to these things.

--- mods/ks_tools/stone_toolheads.lua
+++ mods/ks_tools/stone_toolheads.lua
@@ -1,17 +1,43 @@
 minetest.register_craftitem("ks_tools:toolhead_dolomite_pickaxe", {
 	description = "Dolomite Pickaxe Head",
 	inventory_image = "tools.toolhead_dolomite_pickaxe.png",
-	wield_image = "tools.toolhead_dolomite_pickaxe.png"
+	wield_image = "tools.toolhead_dolomite_pickaxe.png",
+	groups = {stone_toolhead = 1, dolomite_toolhead = 1, pickaxe_toolhead = 1}
 })
 
 minetest.register_craftitem("ks_tools:toolhead_dolomite_shovel", {
 	description = "Dolomite Shovel Head",
 	inventory_image = "tools.toolhead_dolomite_shovel.png",
-	wield_image = "tools.toolhead_dolomite_shovel.png"
+	wield_image = "tools.toolhead_dolomite_shovel.png",
+	groups = {stone_toolhead = 1, dolomite_toolhead = 1, shovel_toolhead = 1}
 })
 
 minetest.register_craftitem("ks_tools:toolhead_dolomite_axe", {
 	description = "Dolomite Axe Head",
 	inventory_image = "tools.toolhead_dolomite_axe.png",
-	wield_image = "tools.toolhead_dolomite_axe.png"
+	wield_image = "tools.toolhead_dolomite_axe.png",
+	groups = {stone_toolhead = 1, dolomite_toolhead = 1, axe_toolhead = 1}
+})
+
+
+
+minetest.register_craftitem("ks_tools:toolhead_aragonite_pickaxe", {
+	description = "Aragonite Pickaxe Head",
+	inventory_image = "tools.toolhead_aragonite_pickaxe.png",
+	wield_image = "tools.toolhead_aragonite_pickaxe.png",
+	groups = {stone_toolhead = 1, aragonite_toolhead = 1, pickaxe_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_aragonite_shovel", {
+	description = "Aragonite Shovel Head",
+	inventory_image = "tools.toolhead_aragonite_shovel.png",
+	wield_image = "tools.toolhead_aragonite_shovel.png",
+	groups = {stone_toolhead = 1, aragonite_toolhead = 1, shovel_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_aragonite_axe", {
+	description = "Aragonite Axe Head",
+	inventory_image = "tools.toolhead_aragonite_axe.png",
+	wield_image = "tools.toolhead_aragonite_axe.png",
+	groups = {stone_toolhead = 1, aragonite_toolhead = 1, axe_toolhead = 1}
 })
