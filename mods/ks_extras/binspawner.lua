804b4dfdba30002f16bc0be3dcec808d763116c0
Eric Rustrum <rustrumek@gmail.com>
2021-09-02 09:46:08 -0700

    Uncompleted code
    For bin spawning routine. This code will turn bin spawners into bins with items.

--- mods/ks_extras/binspawner.lua
+++ mods/ks_extras/binspawner.lua
@@ -1,13 +1,27 @@
 -- { item_name, position_in_inventory }
 local bin_items = {
+	"ks_tools:dolomite_rock",
+	"ks_tools:holly_stick",
 	"ks_tools:holly_adze"
 }
+
+
+local function choose_items(options, chance)
+	local result = {}
+	for i, item in ipairs(options) do
+		if math.random(1,chance) == 1 then
+			result.insert(item) --add item
+		end
+	end
+	return result
+end
+		
 
 
 local function insert_items(pos, listname, items)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
-	inv:add_item("container", ItemStack("ks_tools:holly_stick 3"))
+	inv:add_item("container", ItemStack(""))
 end
 
 
@@ -16,6 +30,7 @@ minetest.register_lbm({
 	name = "ks_extras:bin_spawning",
 	nodenames = "ks_extras:bin_spawner",
 	actions = function(pos, node)
+		insert_item(pos, "container", choose_items(bin_items, 2))
 		minetest.log("chat", "Bin spawned at "..pos.x.." "..pos.y.." "..pos.z)
 	end
 })
