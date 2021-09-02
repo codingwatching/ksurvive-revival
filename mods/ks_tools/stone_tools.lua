fa72a44cbd750cc862e61037258da777a6d7b9b1
Eric Rustrum <rustrumek@gmail.com>
2021-09-02 14:51:17 -0700

    Added Aragonite pickaxes.

--- mods/ks_tools/stone_tools.lua
+++ mods/ks_tools/stone_tools.lua
@@ -141,3 +141,54 @@ minetest.register_tool("ks_tools:holly_dolomite_axe", {
 	visual_scale = 1.69,
 	groups = {stone_tool = 1, dolomite_tool = 1, axe_tool = 1}
 })
+
+
+
+
+
+
+
+minetest.register_tool("ks_tools:juniper_aragonite_pickaxe", {
+	description = "Aragonite Pickaxe",
+	inventory_image = "tools.tool_aragonite_pickaxe_juniper.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			chippable={times={[1]=tool_damage*2, [2]=tool_damage*4}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+	groups = {stone_tool = 1, aragonite_tool = 1, pickaxe_tool = 1}
+})
+
+minetest.register_tool("ks_tools:douglasfir_aragonite_pickaxe", {
+	description = "Aragonite Pickaxe",
+	inventory_image = "tools.tool_aragonite_pickaxe_douglasfir.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			chippable={times={[1]=tool_damage*2, [2]=tool_damage*4}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+	groups = {stone_tool = 1, aragonite_tool = 1, pickaxe_tool = 1}
+})
+
+minetest.register_tool("ks_tools:holly_aragonite_pickaxe", {
+	description = "Aragonite Pickaxe",
+	inventory_image = "tools.tool_aragonite_pickaxe_holly.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			chippable={times={[1]=tool_damage*2, [2]=tool_damage*4}, uses=tool_durability, maxlevel=1},
+		},
+		damage_groups = {fleshy=tool_damage},
+	},
+	visual_scale = 1.69,
+	groups = {stone_tool = 1, aragonite_tool = 1, pickaxe_tool = 1}
+})
