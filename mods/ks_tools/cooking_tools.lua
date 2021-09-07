53a3225e68ccef238ad24e3a97360df5f3981209
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 08:00:55 -0700

    Added the rest of the wooden bowls.

--- mods/ks_tools/cooking_tools.lua
+++ mods/ks_tools/cooking_tools.lua
@@ -4,3 +4,17 @@ minetest.register_craftitem("ks_tools:holly_bowl", {
 	wield_image = "tools.holly_bowl.png",
 	groups = {bowl = 1}
 })
+
+minetest.register_craftitem("ks_tools:juniper_bowl", {
+	description = "Juniper Bowl",
+	inventory_image = "tools.juniper_bowl.png",
+	wield_image = "tools.juniper_bowl.png",
+	groups = {bowl = 1}
+})
+
+minetest.register_craftitem("ks_tools:douglasfir_bowl", {
+	description = "Douglas Fir Bowl",
+	inventory_image = "tools.douglasfir_bowl.png",
+	wield_image = "tools.douglasfir_bowl.png",
+	groups = {bowl = 1}
+})
