8b94199c841466dcd22dfdc5e131b47868340dc1
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 10:54:17 -0700

    Added weaving.lua.

---
+++ mods/ks_flora/weaving.lua
@@ -0,0 +1,15 @@
+minetest.register_craftitem("ks_flora:juncus_stalk", {
+	description = "Juncus Stalk",
+	inventory_image = "flora.juncus_thread.png"
+})
+
+minetest.register_craftitem("ks_flora:juncus_patch", {
+	description = "Patch of Woven Juncus Stalk",
+	inventory_image = "flora.juncus_patch.png"
+})
+
+minetest.register_node("ks_flora:juncus_block", {
+	description = "Block of Woven Juncus Stalk",
+	tiles = {"flora.juncus_block.png"},
+	groups = {sliceable = 1, falling_node = 1}
+})
