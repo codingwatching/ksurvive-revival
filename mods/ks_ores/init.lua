daaa2f7f76cb756ec1064cfa5380bfdfdec813e1
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 13:27:17 -0700

    Added nodes.lua to the modpath listing

--- mods/ks_ores/init.lua
+++ mods/ks_ores/init.lua
@@ -1,3 +1,4 @@
-local default_path = minetest.get_modpath("ks_ores")
+local modpath = minetest.get_modpath("ks_ores")
 
-dofile(default_path.."/minerals.lua")
+dofile(modpath.."/minerals.lua")
+dofile(modpath.."/nodes.lua")
