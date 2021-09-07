47ae3636e3078e02eb924339bf42144b6529350e
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 07:29:02 -0700

    Added more torches.
    Now the douglasfir and juniper torches exist and can be crafted. I do not have any textures for them, yet. I will do that later on.
    
    I would like to find a way to write my own version of walking_lights or illumination mods so that the player can light up nodes when holding torches or lamps.

--- mods/ks_pyro/torches.lua
+++ mods/ks_pyro/torches.lua
@@ -16,3 +16,45 @@ minetest.register_craft({
 	type = "shapeless",
 	recipe = {"ks_tools:holly_stick", "ks_tools:holly_stick"},
 })
+
+
+
+minetest.register_node("ks_pyro:douglasfir_firebrand",{
+	description = "Douglas Fir Firebrand",
+	drawtype = "torchlike",
+	paramtype2="wallmounted",
+	inventory_image = "pyro.douglasfir_firebrand.png",
+	wield_image = "pyro.douglasfir_firebrand.png",
+	tiles = {"pyro.douglasfir_torchup.png", "pyro.douglasfir_torchdown.png", "pyro.douglasfir_torchside.png"},
+	light_source = 12,
+	groups = {grabbable = 1, firebrand = 1, igniter = 1},
+	walkable = false,
+	climbable = true,
+})
+
+minetest.register_craft({
+	output = "ks_pyro:douglasfir_firebrand",
+	type = "shapeless",
+	recipe = {"ks_tools:douglasfir_stick", "ks_tools:douglasfir_stick"},
+})
+
+
+
+minetest.register_node("ks_pyro:juniper_firebrand",{
+	description = "Juniper Firebrand",
+	drawtype = "torchlike",
+	paramtype2="wallmounted",
+	inventory_image = "pyro.juniper_firebrand.png",
+	wield_image = "pyro.juniper_firebrand.png",
+	tiles = {"pyro.juniper_torchup.png", "pyro.juniper_torchdown.png", "pyro.juniper_torchside.png"},
+	light_source = 12,
+	groups = {grabbable = 1, firebrand = 1, igniter = 1},
+	walkable = false,
+	climbable = true,
+})
+
+minetest.register_craft({
+	output = "ks_pyro:juniper_firebrand",
+	type = "shapeless",
+	recipe = {"ks_tools:juniper_stick", "ks_tools:juniper_stick"},
+})
