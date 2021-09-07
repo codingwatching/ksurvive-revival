3815e760a8f6e2a3298310aef1234ede72af12b2
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 10:32:10 -0700

    Created pitch.
    Now, I will have to find a way to make this spawn. Maybe in limestone? I don't really know...
    
    Then again, I could just do the exact same thing as old KSurvive and make it spawn in dolomite.
    
    Better idea: An ore that can be refined into pitch. How about that?
    
    Fun fact: Look up pitch drop experiment and note how long it took for a drip of pitch to drop.

---
+++ mods/ks_pitch/init.lua
@@ -0,0 +1,81 @@
+local pitch_viscosity = 10
+local pitch_range = 1
+
+minetest.register_node("ks_pitch:pitch_source", {
+	description = "Source Pitch",
+	drawtype = "liquid",
+	tiles = {
+		{
+			name = "pitch.pitch.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 8,
+			},
+		},
+		{
+			name = "pitch.pitch.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 8,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	liquidtype = "source",
+	liquid_alternative_flowing = "ks_pitch:pitch_flowing",
+	liquid_alternative_source = "ks_pitch:pitch_source",
+	liquid_viscosity = pitch_viscosity,
+	liquid_range = pitch_range,
+	groups = {pitch = 1, liquid = 3}
+})
+
+minetest.register_node("pitch:pitch_flowing", {
+	description = "Flowing Pitch",
+	drawtype = "flowingliquid",
+	tiles = {
+		{
+			name = "pitch.pitch.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 8,
+			},
+		},
+		{
+			name = "pitch.pitch.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 8,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "ks_pitch:pitch_flowing",
+	liquid_alternative_source = "ks_pitch:pitch_source",
+	liquid_viscosity = pitch_viscosity,
+	liquid_range = pitch_range,
+	groups = {pitch = 1, liquid = 3}
+})
