76aa99c0f2c08760cfb1677e2dc7f9602bae6c41
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 13:10:16 -0700

    Removed falling_node groups.

--- mods/ks_metals/init.lua
+++ mods/ks_metals/init.lua
@@ -1,11 +1,11 @@
 minetest.register_node("ks_metals:aluminium", {
 	description = "Aluminium",
 	tiles = {"metals.aluminium_block.png"},
-	groups = {metal = 1, aluminium = 1, chippable = 2, falling_node = 1},
+	groups = {metal = 1, aluminium = 1, chippable = 2},
 })
 
 minetest.register_node("ks_metals:copper", {
 	description = "Copper",
 	tiles = {"metals.copper_block.png"},
-	groups = {metal = 1, copper = 1, chippable = 2, falling_node = 1},
+	groups = {metal = 1, copper = 1, chippable = 2},
 })
