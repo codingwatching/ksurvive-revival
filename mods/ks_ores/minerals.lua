c219fbcf5a310cbc23dbf0f1c9d5aa5aa36f3694
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 13:47:24 -0700

    Added rose quartz craftitem.

--- mods/ks_ores/minerals.lua
+++ mods/ks_ores/minerals.lua
@@ -9,3 +9,9 @@ minetest.register_craftitem("ks_ores:chalcocite", {
 	inventory_image = "ores.chalcocite.png",
 	wield_image = "ores.chalcocite.png"
 })
+
+minetest.register_craftitem("ks_ores:rose_quartz", {
+	description = "Rose Quartz",
+	inventory_image = "ores.rose_quartz.png",
+	wield_image = "ores.rose_quartz.png"
+})
