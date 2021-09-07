4dd67d6297f7a11dda4b69b660cea546c2073bca
Eric Rustrum <rustrumek@gmail.com>
2021-09-07 11:01:34 -0700

    Changed mapgen.
    Now, temples spawn in temperate forest areas.

--- mods/ks_extras/mapgen.lua
+++ mods/ks_extras/mapgen.lua
@@ -2,7 +2,7 @@ minetest.register_decoration({
 	decoration = "ks_extras:temple_1",
 	deco_type = "schematic",
 	schematic = minetest.get_modpath("ks_extras").."/schematics/temple_1.mts",
-	place_on = "prairie_soil_grass",
+	place_on = "temperate_forest_soil_grass",
 	sidelen = 9,
 	fill_ratio = 0.00005,
 	flags = "place_center_x, place_center_z",
