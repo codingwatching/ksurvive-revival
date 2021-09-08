41ee044f90ac61a6d3589c8e0de867e0a99b5b5b
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 11:01:59 -0700

    Made berries edible.
    I put this off for too long.

--- mods/ks_flora/food.lua
+++ mods/ks_flora/food.lua
@@ -1,11 +1,17 @@
 minetest.register_craftitem("ks_flora:juniper_berries", {
 	description = "Gin Berries",
 	inventory_image = "flora.juniper_berries.png",
-	wield_image = "flora.juniper_berries.png"
+	wield_image = "flora.juniper_berries.png",
+	on_use = function()
+		minetest.item_eat(4)
+	end
 })
 
 minetest.register_craftitem("ks_flora:holly_berries", {
 	description = "Holly Berries",
 	inventory_image = "flora.holly_berries.png",
-	wield_image = "flora.holly_berries.png"
+	wield_image = "flora.holly_berries.png",
+	on_use = function()
+		minetest.item_eat(4)
+	end
 })
