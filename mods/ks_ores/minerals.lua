35f86eb5db32278a93f83e42d22e9b16cc2a8372
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 09:40:54 -0700

    Added fluorite crystal.
    Also renamed rose quartz.

--- mods/ks_ores/minerals.lua
+++ mods/ks_ores/minerals.lua
@@ -11,7 +11,22 @@ minetest.register_craftitem("ks_ores:chalcocite", {
 })
 
 minetest.register_craftitem("ks_ores:rose_quartz", {
-	description = "Rose Quartz",
+	description = "Rose Quartz Crystal",
 	inventory_image = "ores.rose_quartz.png",
 	wield_image = "ores.rose_quartz.png"
+})
+
+minetest.register_craftitem("ks_ores:fluorite", {
+	description = "Fluorite Crystal",
+	inventory_image = "ores.fluorite.png",
+	wield_image = "ores.fluorite.png"
+})
+
+minetest.register_node("ks_ores:fluorite_spike", {
+	description = "Fluorite Spike",
+	drawtype = "plantlike",
+	inventory_image = "ores.fluorite_spike.png",
+	wield_image = "ores.fluorite_spike.png",
+	tiles = {"ores.fluorite_spike.png"},
+	groups = {chippable = 1}
 })
