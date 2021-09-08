588b01f928ae944179b5798620749ccb2b4e802d
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 11:05:36 -0700

    Fixed another boneheaded typo
    Also added holly berry generation. This is cool.

--- mods/ks_flora/abm.lua
+++ mods/ks_flora/abm.lua
@@ -2,11 +2,21 @@ local berry_growth_rate = 1
 local berry_grow_chance = 1
 
 minetest.register_abm({
-	nodenames = {"ks_flora:juniper_leaves_living"},
+	nodenames = {"ks_flora:juniper_living_leaves"},
 	interval = berry_growth_rate,
 	chance = berry_grow_chance,
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		local pos = {x = pos.x, y = pos.y, z = pos.z}
 		minetest.set_node(pos, {name = "ks_flora:juniper_living_leaves_with_berries"})
+	end
+})
+
+minetest.register_abm({
+	nodenames = {"ks_flora:holly_living_leaves"},
+	interval = berry_growth_rate,
+	chance = berry_grow_chance,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local pos = {x = pos.x, y = pos.y, z = pos.z}
+		minetest.set_node(pos, {name = "ks_flora:holly_living_leaves_with_berries"})
 	end
 })
