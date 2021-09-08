b1a3d4db539998ad77cdfbffc49091dadb012bfb
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 14:25:43 -0700

    Made texture animate.
    Now, the texture animates. Also added groups. And fixed whitespace issues.

--- mods/ks_pyro/init.lua
+++ mods/ks_pyro/init.lua
@@ -1,10 +1,22 @@
 local modpath = minetest.get_modpath("ks_pyro")
 
 minetest.register_node("ks_pyro:flame", {
-	    description = "Open Flame",
-	    drawtype = "firelike",
-	    drop = {},
-	    tiles = {"pyro.flame.png"}
+	description = "Open Flame",
+	drawtype = "firelike",
+	drop = {},
+	tiles = {
+		{
+			name = "pyro.flame.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			}
+		}
+	},
+	groups = {flame = 1, igniter = 1}
 }
 
 dofile(modpath.."/torches.lua")
