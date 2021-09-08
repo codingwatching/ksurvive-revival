4216e6a1c2039b30062d2e67ec7a5f181043e9a0
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 14:20:30 -0700

    Added flame node.

--- mods/ks_pyro/init.lua
+++ mods/ks_pyro/init.lua
@@ -1,3 +1,10 @@
 local modpath = minetest.get_modpath("ks_pyro")
 
+minetest.register_node("ks_pyro:flame", {
+	    description = "Open Flame",
+	    drawtype = "firelike",
+	    drop = {},
+	    tiles = {"pyro.flame.png"}
+}
+
 dofile(modpath.."/torches.lua")
