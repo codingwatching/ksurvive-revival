6959251d10b7c76a97e04d34020dcce800500c8c
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 09:57:21 -0700

    Added ores mapgen.lua
    Now, fluorite spikes should naturally spawn in caves.

---
+++ mods/ks_ores/mapgen.lua
@@ -0,0 +1,9 @@
+minetest.register_decoration({
+	decoration = "ks_ores:fluorite_spike",
+	deco_type = "simple",
+	place_on = "fluorite_cave_floor",
+	sidelen = 9,
+	fill_ratio = 0.05,
+	flags = "place_center_x, place_center_z",
+	rotation = "random",
+})
