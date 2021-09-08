08fe0f4286cf02c994f695f00ae8c534f86e968e
Eric Rustrum <rustrumek@gmail.com>
2021-09-08 11:03:20 -0700

    Fixed boneheaded mistake
    Speaking of boneheaded, have you played KSurvive?

--- mods/ks_flora/food.lua
+++ mods/ks_flora/food.lua
@@ -2,16 +2,12 @@ minetest.register_craftitem("ks_flora:juniper_berries", {
 	description = "Gin Berries",
 	inventory_image = "flora.juniper_berries.png",
 	wield_image = "flora.juniper_berries.png",
-	on_use = function()
-		minetest.item_eat(4)
-	end
+	on_use = minetest.item_eat(4)
 })
 
 minetest.register_craftitem("ks_flora:holly_berries", {
 	description = "Holly Berries",
 	inventory_image = "flora.holly_berries.png",
 	wield_image = "flora.holly_berries.png",
-	on_use = function()
-		minetest.item_eat(4)
-	end
+	on_use = minetest.item_eat(4)
 })
