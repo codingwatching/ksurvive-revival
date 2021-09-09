02cafdf58c34c6f5140d2cbe060547e0e429130c
Eric Rustrum <rustrumek@gmail.com>
2021-09-09 07:35:33 -0700

    Made fluorite spikes form on fluorite ore.
    Yep. Made up my mind already.

--- mods/ks_ores/mapgen.lua
+++ mods/ks_ores/mapgen.lua
@@ -1,9 +1,9 @@
 minetest.register_decoration({
 	decoration = "ks_ores:fluorite_spike",
 	deco_type = "simple",
-	place_on = "fluorite_cave_floor",
+	place_on = "mineral_limestone_fluorite",
 	sidelen = 9,
-	fill_ratio = 0.01,
+	fill_ratio = 1,
 	flags = "place_center_x, place_center_z",
 	rotation = "random",
 })
