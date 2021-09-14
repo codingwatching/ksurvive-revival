a3ad0916fd781ade7e73005a91eff796b82093fd
Eric Rustrum <rustrumek@gmail.com>
2021-09-14 10:39:21 -0700

    Added alumina dust node.

--- mods/ks_metals/nodes.lua
+++ mods/ks_metals/nodes.lua
@@ -9,3 +9,25 @@ minetest.register_node("ks_metals:copper", {
 	tiles = {"metals.copper_block.png"},
 	groups = {metal = 1, solid_metal = 1, copper = 1, chippable = 2},
 })
+
+minetest.register_node("ks_metals:alumina_dust", {
+	description = "Dust of Alumina",
+	tiles = {"metals.alumina.png"},
+	inventory_image = "metals.alumina_dust.png",
+	wield_image = "metals.alumina_dust.png",
+	paramtype = "light",
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.25, 0.5},
+		},
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -6 / 16, 0.5},
+		},
+	},
+	groups = {diggable = 1, falling_node = 1, alumina = 1},
+})
