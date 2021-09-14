339c58374df59980662a24f343ee1b27f91cdedb
Eric Rustrum <rustrumek@gmail.com>
2021-09-14 10:41:55 -0700

    Removed obsolete nodes.

--- mods/ks_ores/nodes.lua
+++ mods/ks_ores/nodes.lua
@@ -1,15 +1,3 @@
-minetest.register_node("ks_ores:bauxite_block", {
-	description = "Block of Bauxite",
-	tiles = {"ores.bauxite_block.png"},
-	groups = {bauxite = 1, chippable = 1, falling_node = 1},
-})
-
-minetest.register_node("ks_ores:chalcocite_block", {
-	description = "Block of Chalcocite",
-	tiles = {"ores.chalcocite_block.png"},
-	groups = {chalcocite = 1, chippable = 1, falling_node = 1},
-})
-
 minetest.register_node("ks_ores:rose_quartz_block", {
 	description = "Block of Rose Quartz",
 	tiles = {"ores.rose_quartz_block.png"},
@@ -20,26 +8,6 @@ minetest.register_node("ks_ores:fluorite_block", {
 	description = "Block of Fluorite",
 	tiles = {"ores.fluorite_block.png"},
 	groups = {fluorite = 1, chippable = 1, falling_node = 1},
-})
-
-
-
-minetest.register_craft({
-	recipe = {
-		{"ks_ores:bauxite", "ks_ores:bauxite", "ks_ores:bauxite"},
-		{"ks_ores:bauxite", "ks_ores:bauxite", "ks_ores:bauxite"},
-		{"ks_ores:bauxite", "ks_ores:bauxite", "ks_ores:bauxite"}
-	},
-	output = "ks_ores:bauxite_block"
-})
-
-minetest.register_craft({
-	recipe = {
-		{"ks_ores:chalcocite", "ks_ores:chalcocite", "ks_ores:chalcocite"},
-		{"ks_ores:chalcocite", "ks_ores:chalcocite", "ks_ores:chalcocite"},
-		{"ks_ores:chalcocite", "ks_ores:chalcocite", "ks_ores:chalcocite"}
-	},
-	output = "ks_ores:chalcocite_block"
 })
 
 minetest.register_craft({
