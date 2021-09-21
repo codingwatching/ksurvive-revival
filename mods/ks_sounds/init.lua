659c1a1654cb05cb38d14c3b8032badd226c46ed
Eric Rustrum <rustrumek@gmail.com>
2021-09-21 14:11:01 -0700

    Added sound subgame mod.
    This will be where sounds are handled.

---
+++ mods/ks_sounds/init.lua
@@ -0,0 +1,12 @@
+ks_sounds = {}
+
+function ks_sounds.default_node_sounds(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name = "", gain = 1.0}
+	table.dug = table.dug or
+			{name = "default_dug_node", gain = 0.25}
+	table.place = table.place or
+			{name = "default_place_node_hard", gain = 1.0}
+	return table
+end
