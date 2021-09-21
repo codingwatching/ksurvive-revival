c267bb8b6f33dd82a231621f38d73e9983f099ca
Eric Rustrum <rustrumek@gmail.com>
2021-09-21 14:14:59 -0700

    Updated init.lua

--- mods/ks_sounds/init.lua
+++ mods/ks_sounds/init.lua
@@ -1,12 +1,10 @@
 ks_sounds = {}
 
+-- Basically just code from MTG default mod.
 function ks_sounds.default_node_sounds(table)
 	table = table or {}
-	table.footstep = table.footstep or
-			{name = "", gain = 1.0}
-	table.dug = table.dug or
-			{name = "default_dug_node", gain = 0.25}
-	table.place = table.place or
-			{name = "default_place_node_hard", gain = 1.0}
+	table.footstep = table.footstep or {name = "", gain = 1.0}
+	table.dug = table.dug or {name = "default_dug_node", gain = 0.25}
+	table.place = table.place or {name = "default_place_node_hard", gain = 1.0}
 	return table
 end
