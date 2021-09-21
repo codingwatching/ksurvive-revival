020dde7c469a2a221ac67023f312643bc55274ad
Eric Rustrum <rustrumek@gmail.com>
2021-09-21 14:18:21 -0700

    Added link to source
    Gotta continue this later. Laptop's running low on battery.

--- mods/ks_sounds/init.lua
+++ mods/ks_sounds/init.lua
@@ -4,7 +4,10 @@ ks_sounds = {}
 function ks_sounds.default_node_sounds(table)
 	table = table or {}
 	table.footstep = table.footstep or {name = "", gain = 1.0}
-	table.dug = table.dug or {name = "default_dug_node", gain = 0.25}
-	table.place = table.place or {name = "default_place_node_hard", gain = 1.0}
+	table.dug = table.dug or {name = "sounds.generalnode.dug", gain = 0.25}
+	table.place = table.place or {name = "sounds.generalnode.place", gain = 1.0}
 	return table
 end
+
+-- Original code: https://github.com/minetest/minetest_game/blob/master/mods/default/functions.lua
+-- TODO: Examine this code and find out what makes it tick.
