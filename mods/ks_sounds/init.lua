d72012586c9025ca1b961deda036a9f7dab093fb
Eric Rustrum <rustrumek@gmail.com>
2021-09-23 09:44:34 -0700

    Added some template sounds.
    These are just so I don't have to keep listening to the same damn sounds all the time.

--- mods/ks_sounds/init.lua
+++ mods/ks_sounds/init.lua
@@ -4,3 +4,27 @@ ks_sounds.generalnode_sounds = {}
 ks_sounds.generalnode_sounds.footstep = {name = "generalnode_footstep", gain = 1.0}
 ks_sounds.generalnode_sounds.dug = {name = "generalnode_dug", gain = 0.25}
 ks_sounds.generalnode_sounds.place = {name = "generalnode_place", gain = 1.0}
+
+
+ks_sounds.woodennode_sounds = {}
+ks_sounds.woodennode_sounds.footstep = {name = "woodennode_footstep", gain = 1.0}
+ks_sounds.woodennode_sounds.dug = {name = "woodennode_dug", gain = 0.25}
+ks_sounds.woodennode_sounds.place = {name = "woodennode_place", gain = 1.0}
+
+
+ks_sounds.leafynode_sounds = {}
+ks_sounds.leafynode_sounds.footstep = {name = "leafynode_footstep", gain = 1.0}
+ks_sounds.leafynode_sounds.dug = {name = "leafynode_dug", gain = 0.25}
+ks_sounds.leafynode_sounds.place = {name = "leafynode_place", gain = 1.0}
+
+
+ks_sounds.bedrocknode_sounds = {}
+ks_sounds.bedrocknode_sounds.footstep = {name = "bedrocknode_footstep", gain = 1.0}
+ks_sounds.bedrocknode_sounds.dug = {name = "bedrocknode_dug", gain = 0.25}
+ks_sounds.bedrocknode_sounds.place = {name = "bedrocknode_place", gain = 1.0}
+
+
+ks_sounds.snowynode_sounds = {}
+ks_sounds.snowynode_sounds.footstep = {name = "snowynode_footstep", gain = 1.0}
+ks_sounds.snowynode_sounds.dug = {name = "snowynode_dug", gain = 0.25}
+ks_sounds.snowynode_sounds.place = {name = "snowynode_place", gain = 1.0}
