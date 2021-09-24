e1177e16603c38a02005eca53da152438a4947af
Eric Rustrum <rustrumek@gmail.com>
2021-09-24 08:56:11 -0700

    Made fire buildable to.
    Also added a TODO comment for an ABM that kills a fire if it doesn't have what a fire needs to live.

--- mods/ks_pyro/init.lua
+++ mods/ks_pyro/init.lua
@@ -20,6 +20,13 @@ minetest.register_node("ks_pyro:flame", {
 	groups = {flame = 1, igniter = 1, smelter = 1},
 	walkable = false,
 	pointable = false,
+	buildable_to = true,
 })
+
+-- TODO: add ABM to remove a flame if there is nothing to sustain it. Things a flame needs to survive:
+--
+-- A: Air
+-- B: A flammable node nearby
+-- C: Heat (Going to be pretty hard to implement. Maybe it could be biome-specific? Like a fire is harder to have in a cold desert.)
 
 dofile(modpath.."/torches.lua")
