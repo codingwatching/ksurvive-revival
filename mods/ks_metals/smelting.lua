2fe52ac09dc922cfc179c9cf439592642e7e0ce6
Eric Rustrum <rustrumek@gmail.com>
2021-09-24 07:58:01 -0700

    Fixed typo.
    This causes the game to crash because 'ks_metals:aluminum" is spelled wrong.

--- mods/ks_metals/smelting.lua
+++ mods/ks_metals/smelting.lua
@@ -27,6 +27,6 @@ minetest.register_abm({
 	chance = forming_chance,
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		local pos = {x = pos.x, y = pos.y, z = pos.z}
-		minetest.set_node(pos, {name = "ks_metals:aluminum"})
+		minetest.set_node(pos, {name = "ks_metals:aluminium"})
 	end
 })
