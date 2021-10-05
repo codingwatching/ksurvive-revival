3bef86400eb9841713d7c24acdd4cb6eb1a38d7f
Eric Rustrum <48301203+MadKamel@users.noreply.github.com>
2021-10-05 15:33:13 -0700

    Added experimental leaf-decay
    This should make it so that leaf nodes not near living logs will always decay.

--- mods/ks_flora/leafdecay.lua
+++ mods/ks_flora/leafdecay.lua
@@ -1,11 +1,37 @@
+local distance = 3
+
 minetest.register_abm({
 	label = "Remove lonely juniper leaves.",
 	nodenames = {"group:juniper_leaves"},
 	interval = 1,
 	chance = 1,
-	action = function(pos, node, active_object_count,
-		active_object_count_wider)
-		local pos = {x = pos.x, y = pos.y, z = pos.z}
-		minetest.set_node(pos, {name = "air"})
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		if minetest.find_node_near(pos, distance, { "ks_flora:juniper_living_log" }) == nil then
+			minetest.set_node(pos, {name = "air"})
+		end
+	end
+})
+
+minetest.register_abm({
+	label = "Remove lonely holly leaves.",
+	nodenames = {"group:holly_leaves"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		if minetest.find_node_near(pos, distance, { "ks_flora:holly_living_log" }) == nil then
+			minetest.set_node(pos, {name = "air"})
+		end
+	end
+})
+
+minetest.register_abm({
+	label = "Remove lonely douglas fir leaves.",
+	nodenames = {"group:douglasfir_leaves"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		if minetest.find_node_near(pos, distance, { "ks_flora:douglasfir_living_log" }) == nil then
+			minetest.set_node(pos, {name = "air"})
+		end
 	end
 })
