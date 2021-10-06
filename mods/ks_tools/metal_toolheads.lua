2aa89605084dd6dbed7d513d6f6d0c7558d119a3
Eric Rustrum <rustrumek@gmail.com>
2021-10-06 12:27:07 -0700

    Added metal toolheads.
    Now these are registered, but there are no actual textures for them yet...

---
+++ mods/ks_tools/metal_toolheads.lua
@@ -0,0 +1,43 @@
+minetest.register_craftitem("ks_tools:toolhead_aluminium_pickaxe", {
+	description = "Aluminium Pickaxe Head",
+	inventory_image = "tools.toolhead_aluminium_pickaxe.png",
+	wield_image = "tools.toolhead_aluminium_pickaxe.png",
+	groups = {metal_toolhead = 1, aluminium_toolhead = 1, pickaxe_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_aluminium_shovel", {
+	description = "Aluminium Shovel Head",
+	inventory_image = "tools.toolhead_aluminium_shovel.png",
+	wield_image = "tools.toolhead_aluminium_shovel.png",
+	groups = {metal_toolhead = 1, aluminium_toolhead = 1, shovel_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_aluminium_axe", {
+	description = "Aluminium Axe Head",
+	inventory_image = "tools.toolhead_aluminium_axe.png",
+	wield_image = "tools.toolhead_aluminium_axe.png",
+	groups = {metal_toolhead = 1, aluminium_toolhead = 1, axe_toolhead = 1}
+})
+
+
+
+minetest.register_craftitem("ks_tools:toolhead_copper_pickaxe", {
+	description = "Copper Pickaxe Head",
+	inventory_image = "tools.toolhead_copper_pickaxe.png",
+	wield_image = "tools.toolhead_copper_pickaxe.png",
+	groups = {metal_toolhead = 1, copper_toolhead = 1, pickaxe_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_copper_shovel", {
+	description = "Copper Shovel Head",
+	inventory_image = "tools.toolhead_copper_shovel.png",
+	wield_image = "tools.toolhead_copper_shovel.png",
+	groups = {metal_toolhead = 1, copper_toolhead = 1, shovel_toolhead = 1}
+})
+
+minetest.register_craftitem("ks_tools:toolhead_copper_axe", {
+	description = "Copper Axe Head",
+	inventory_image = "tools.toolhead_copper_axe.png",
+	wield_image = "tools.toolhead_copper_axe.png",
+	groups = {metal_toolhead = 1, copper_toolhead = 1, axe_toolhead = 1}
+})
