d589cd728f8b78fd21599430d10f0dca827e0523
Eric Rustrum <rustrumek@gmail.com>
2023-03-25 13:16:48 -0700

    Made alumina less costly.

--- mods/ks_metals/nodes.lua
+++ mods/ks_metals/nodes.lua
@@ -18,9 +18,8 @@ minetest.register_node("ks_metals:alumina", {
 
 minetest.register_craft({
 	recipe = {
-		{"ks_metals:alumina_dust", "ks_metals:alumina_dust", "ks_metals:alumina_dust"},
-		{"ks_metals:alumina_dust", "ks_metals:alumina_dust", "ks_metals:alumina_dust"},
-		{"ks_metals:alumina_dust", "ks_metals:alumina_dust", "ks_metals:alumina_dust"}
+		{"ks_metals:alumina_dust", "ks_metals:alumina_dust"},
+		{"ks_metals:alumina_dust", "ks_metals:alumina_dust"}
 	},
 	output = "ks_metals:alumina"
 })
